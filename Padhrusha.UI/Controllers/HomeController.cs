a5b1e6e0bd38757acea278f0331d0a115035fea0
Dzmitry Padhrusha <dzmitry.padhrusha@krafteq.de>
2025-03-11 10:09:13 +0300

    Add project files.

---
+++ Padhrusha.UI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Padhrusha.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
